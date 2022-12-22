4834933d34245b2e46e28569800fa013cf256c41
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-12-22 17:56:40 -0500

    Update SlimeMixin.java

--- src/main/java/net/lunade/slime/mixin/SlimeMixin.java
+++ src/main/java/net/lunade/slime/mixin/SlimeMixin.java
@@ -3,7 +3,6 @@ package net.lunade.slime.mixin;
 import net.lunade.slime.SlimeMethods;
 import net.lunade.slime.impl.SlimeInterface;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.Component;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
