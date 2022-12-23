5e589d148d5ff8788289313001379ba5cc7dda5c
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-12-23 18:49:41 -0500

    Update BaseSpawnerMixin.java

--- src/main/java/net/lunade/slime/mixin/BaseSpawnerMixin.java
+++ src/main/java/net/lunade/slime/mixin/BaseSpawnerMixin.java
@@ -1,6 +1,8 @@
 package net.lunade.slime.mixin;
 
 import net.lunade.slime.impl.SlimeInterface;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.monster.Slime;
 import net.minecraft.world.level.BaseSpawner;
@@ -18,7 +20,7 @@ public class BaseSpawnerMixin {
     @Shadow @Nullable private Entity displayEntity;
 
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/EntityType;loadEntityRecursive(Lnet/minecraft/nbt/CompoundTag;Lnet/minecraft/world/level/Level;Ljava/util/function/Function;)Lnet/minecraft/world/entity/Entity;", shift = At.Shift.AFTER), method = "getOrCreateDisplayEntity")
-    public void modifySlimeSize(Level level, CallbackInfoReturnable<Entity> info) {
+    public void modifySlimeSize(Level level, RandomSource randomSource, BlockPos blockPos, CallbackInfoReturnable<Entity> info) {
         if (this.displayEntity instanceof Slime) {
             ((SlimeInterface) this.displayEntity).spawnerSizeFix(1F);
         }
