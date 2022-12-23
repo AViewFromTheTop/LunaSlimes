6f8809eb0a1195ab68b69020ec9a282bd85eb063
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-12-22 21:13:36 -0500

    test

--- src/main/java/net/lunade/slime/mixin/client/MagmaCubeRendererMixin.java
+++ src/main/java/net/lunade/slime/mixin/client/MagmaCubeRendererMixin.java
@@ -10,17 +10,14 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(MagmaCubeRenderer.class)
 public class MagmaCubeRendererMixin {
 
-    @Unique float hI;
-
-    @Inject(at = @At("HEAD"), method = "scale")
+    @Inject(at = @At("HEAD"), method = "scale", cancellable = true)
     public void anims(MagmaCube slime, PoseStack poseStack, float f, CallbackInfo info) {
+        info.cancel();
         float splitAnimProgress = ((SlimeInterface)slime).splitAnimProgress(f);
         float splitValue = (float) (((splitAnimProgress + (0.0955F * Math.PI)) * Math.PI) * 5F);
         float splitAnimXZ = (float) ((Math.cos(splitValue) * 0.1F) + 1F);
@@ -36,14 +33,8 @@ public class MagmaCubeRendererMixin {
 
         float i = ((SlimeInterface)slime).getSizeScale(f);
         float g = Mth.lerp(f, ((SlimeInterface)slime).prevSquish(), slime.squish) / ((i) * 0.5f + 1.0f);
-        this.hI = (1.0F / (g + 1.0F)) * i;
-    }
-
-    @ModifyArgs(at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;scale(FFF)V"), method = "scale")
-    public void setScaleArgs(Args args) {
-        args.set(0, hI);
-        args.set(1, 1F / hI);
-        args.set(2, hI);
+        float hI = (1.0F / (g + 1.0F)) * i;
+        poseStack.scale(hI, 1.0F / hI, hI);
     }
 
 }
