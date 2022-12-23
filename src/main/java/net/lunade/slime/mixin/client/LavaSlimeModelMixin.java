1107daad4d6ea20f8e4d817893e29ced3be3302f
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-12-22 21:08:42 -0500

    model

---
+++ src/main/java/net/lunade/slime/mixin/client/LavaSlimeModelMixin.java
@@ -0,0 +1,19 @@
+package net.lunade.slime.mixin.client;
+
+import net.lunade.slime.impl.SlimeInterface;
+import net.minecraft.client.model.LavaSlimeModel;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.monster.Slime;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(LavaSlimeModel.class)
+public class LavaSlimeModelMixin<T extends Slime> {
+
+    @ModifyVariable(at = @At("STORE"), method = "prepareMobModel", ordinal = 3)
+    public float prepareMobModel(float original, T slime, float f, float g, float h) {
+        return Mth.lerp(h, ((SlimeInterface)slime).prevSquish(), slime.squish);
+    }
+
+}
