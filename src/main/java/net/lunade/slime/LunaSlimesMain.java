b77094b03b59e477b00a89be1a1dc32ede142396
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-12-24 12:09:19 -0500

    Fix Client 1.19.2 Stuff I Guess Thanks QuiltFlower

--- src/main/java/net/lunade/slime/LunaSlimesMain.java
+++ src/main/java/net/lunade/slime/LunaSlimesMain.java
@@ -3,7 +3,6 @@ package net.lunade.slime;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 
@@ -12,13 +11,13 @@ public class LunaSlimesMain implements ModInitializer {
 	public static final boolean HAS_CLOTH_CONFIG = FabricLoader.getInstance().isModLoaded("cloth-config");
 	public static boolean areConfigsInit;
 
-	public static final SoundEvent SLIME_MERGE = SoundEvent.createVariableRangeEvent(new ResourceLocation("lunaslimes", "entity.slime.merge"));
-	public static final SoundEvent SLIME_SPLIT = SoundEvent.createVariableRangeEvent(new ResourceLocation("lunaslimes", "entity.slime.split"));
+	public static final SoundEvent SLIME_MERGE = new SoundEvent(new ResourceLocation("lunaslimes", "entity.slime.merge"));
+	public static final SoundEvent SLIME_SPLIT = new SoundEvent(new ResourceLocation("lunaslimes", "entity.slime.split"));
 
 	@Override
 	public void onInitialize() {
-		Registry.register(BuiltInRegistries.SOUND_EVENT, new ResourceLocation("lunaslimes", "entity.slime.merge"), SLIME_MERGE);
-		Registry.register(BuiltInRegistries.SOUND_EVENT, new ResourceLocation("lunaslimes", "entity.slime.split"), SLIME_SPLIT);
+		Registry.register(Registry.SOUND_EVENT, new ResourceLocation("lunaslimes", "entity.slime.merge"), SLIME_MERGE);
+		Registry.register(Registry.SOUND_EVENT, new ResourceLocation("lunaslimes", "entity.slime.split"), SLIME_SPLIT);
 	}
 
 }
