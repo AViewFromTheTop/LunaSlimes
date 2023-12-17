1731529eae228137152beaca07076e58b55c2678
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-12-16 22:13:21 -0500

    mixinextras is pog

--- src/main/java/net/lunade/slime/mixin/SlimeMixin.java
+++ src/main/java/net/lunade/slime/mixin/SlimeMixin.java
@@ -21,17 +21,19 @@ import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.SpawnGroupData;
-import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.monster.Slime;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.Blocks;
 import org.jetbrains.annotations.Nullable;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -224,10 +226,20 @@ public class SlimeMixin implements SlimeInterface {
         entityData.set(LUNASLIMES$CURRENT_SIZE, 0F);
     }
 
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/ai/attributes/AttributeInstance;setBaseValue(D)V", ordinal = 0, shift = At.Shift.AFTER), method = "setSize")
-    public void lunaSlimes$oddHealth(int i, boolean bl, CallbackInfo info) {
-        int clampedSize = Mth.clamp(i, 1, 127);
-        Slime.class.cast(this).getAttribute(Attributes.MAX_HEALTH).setBaseValue(clampedSize % 2 == 0 ? clampedSize * clampedSize : clampedSize);
+    @WrapOperation(
+            method = "setSize",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/ai/attributes/AttributeInstance;setBaseValue(D)V", ordinal = 0),
+            slice = @Slice(
+                    from = @At(
+                            value = "FIELD",
+                            target = "Lnet/minecraft/world/entity/ai/attributes/Attributes;MAX_HEALTH:Lnet/minecraft/world/entity/ai/attributes/Attribute;",
+                            opcode = Opcodes.GETSTATIC
+                    )
+            )
+    )
+    public void lunaSlimes$oddHealth(AttributeInstance attributeInstance, double value, Operation<Void> operation) {
+        int sqrt = (int) Math.sqrt(value);
+        operation.call(attributeInstance, sqrt % 2 == 0 ? value : sqrt);
     }
 
     @Inject(at = @At("HEAD"), method = "decreaseSquish", cancellable = true)
