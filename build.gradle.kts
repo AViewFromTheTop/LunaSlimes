a5c117aa8c7e40c30dd6386c653e71096bc4267a
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-06-14 00:53:31 -0400

    1.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -169,7 +169,7 @@ tasks {
         val properties = mapOf(
             "mod_id" to mod_id,
             "version" to version,
-            "minecraft_version" to minecraft_version,
+            "minecraft_version" to ">=$minecraft_version",
 
             "fabric_api_version" to ">=$fabric_api_version",
             "frozenlib_version" to ">=${frozenlib_version.split('-').firstOrNull()}-"
