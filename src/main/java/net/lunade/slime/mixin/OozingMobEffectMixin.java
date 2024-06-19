0c6ba8ae6e316cef420723c5235272aa146a78f3
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-06-19 12:51:00 -0400

    Fix Oozing

---
+++ src/main/java/net/lunade/slime/mixin/OozingMobEffectMixin.java
@@ -0,0 +1,30 @@
+package net.lunade.slime.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.lunade.slime.impl.SlimeInterface;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.monster.Slime;
+import net.minecraft.world.effect.OozingMobEffect;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(OozingMobEffect.class)
+public class OozingMobEffectMixin {
+
+    @WrapOperation(
+		method = "spawnSlimeOffspring",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/world/level/Level;addFreshEntity(Lnet/minecraft/world/entity/Entity;)Z"
+		)
+	)
+    public boolean lunaSlimes$spawnSlimeOffspring(Level instance, Entity entity, Operation<Boolean> original) {
+		if (entity instanceof Slime slime) {
+			((SlimeInterface)slime).lunaSlimes$setMergeCooldown(100);
+		}
+		return original.call(instance, entity);
+	}
+
+}
